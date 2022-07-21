6d2402b583c85033b4dfe8ed82bd497803cd083b
Jorge Rodriguez <jarf4321@gmail.com>
2022-07-21 04:56:05 -0500

    Add configurations to syntax icons and puts on right side when open a new windows

--- lua/settings/init.lua
+++ lua/settings/init.lua
@@ -11,3 +11,5 @@ set.softtabstop = 2        --"To make 2 spaces as Tab
 set.tabstop = 2		   --"To make 2 spaces as Tab
 set.expandtab = true       --"To replace tabs with whitespaces"
 set.fileencoding = 'utf-8'
+set.syntax = 'on'         -- In order to apply syntax on text and icons
+set.splitright = true   -- To split new buffers on the right side of the window
