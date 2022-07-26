8f779da3fae452303975a0f1da0105e0dd7bb8f7
Jorge Rodriguez <jarf4321@gmail.com>
2022-07-26 10:40:31 -0500

    Update alpha dashboard logo

--- lua/user/alpha.lua
+++ lua/user/alpha.lua
@@ -5,12 +5,17 @@ end
 
 local dashboard = require "alpha.themes.dashboard"
 dashboard.section.header.val = {
-  [[                               __                ]],
-  [[  ___     ___    ___   __  __ /\_\    ___ ___    ]],
-  [[ / _ `\  / __`\ / __`\/\ \/\ \\/\ \  / __` __`\  ]],
-  [[/\ \/\ \/\  __//\ \_\ \ \ \_/ |\ \ \/\ \/\ \/\ \ ]],
-  [[\ \_\ \_\ \____\ \____/\ \___/  \ \_\ \_\ \_\ \_\]],
-  [[ \/_/\/_/\/____/\/___/  \/__/    \/_/\/_/\/_/\/_/]],
+"         ─▄▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▄",
+"         █░░░█░░░░░░░░░░▄▄░██░█",
+"         █░▀▀█▀▀░▄▀░▄▀░░▀▀░▄▄░█",
+"         █░░░▀░░░▄▄▄▄▄░░██░▀▀░█",
+"         ─▀▄▄▄▄▄▀─────▀▄▄▄▄▄▄▀",
+" ╔═╗ ╔╗ ╔═══╗ ╔═══╗ ╔╗  ╔╗ ╔══╗ ╔═╗╔═╗",
+" ║║╚╗║║ ║╔══╝ ║╔═╗║ ║╚╗╔╝║ ╚╣╠╝ ║║╚╝║║",
+" ║╔╗╚╝║ ║╚══╗ ║║ ║║ ╚╗║║╔╝  ║║  ║╔╗╔╗║",
+" ║║╚╗║║ ║╔══╝ ║║ ║║  ║╚╝║   ║║  ║║║║║║",
+" ║║ ║║║ ║╚══╗ ║╚═╝║  ╚╗╔╝  ╔╣╠╗ ║║║║║║",
+" ╚╝ ╚═╝ ╚═══╝ ╚═══╝   ╚╝   ╚══╝ ╚╝╚╝╚╝",
 }
 dashboard.section.buttons.val = {
   dashboard.button("f", " " .. " Find file", ":Telescope find_files <CR>"),
@@ -21,11 +26,6 @@ dashboard.section.buttons.val = {
   dashboard.button("c", " " .. " Config", ":e ~/.config/nvim/init.lua <CR>"),
   dashboard.button("q", " " .. " Quit", ":qa<CR>"),
 }
-local function footer()
-  return "chrisatmachine.com"
-end
-
-dashboard.section.footer.val = footer()
 
 dashboard.section.footer.opts.hl = "Type"
 dashboard.section.header.opts.hl = "Include"
