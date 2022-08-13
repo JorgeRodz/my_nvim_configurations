39b4d32a2de2c27c9746c41bfe128f0128dbe8a0
Jorge Rodriguez <jarf4321@gmail.com>
2022-08-13 18:34:31 -0500

    Add: code-runner default configuration

---
+++ lua/user/code-runner.lua
@@ -0,0 +1,13 @@
+local status_ok, code_runner = pcall(require, "code_runner")
+if not status_ok then
+  return
+end
+
+code_runner.setup {
+  -- Default setup
+  mode = "toggleterm",
+  focus = false,
+  filetype = {
+    ruby = "ruby"
+  }
+}
