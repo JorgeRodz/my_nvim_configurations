7682962c0d5ee299c2bdbab1525b4cc965882472
Jorge Rodriguez <jarf4321@gmail.com>
2022-08-13 18:55:52 -0500

    Modified - add new keymaps for plugins:
      - bufferline go to type number
      - null-ls show diasnostic
      - code-runner keymaps for run code on toggleterm

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -40,10 +40,10 @@ keymap("n", "<C-k>", "<C-w>k", opts)
 keymap("n", "<C-l>", "<C-w>l", opts)
 
 -- Resize with arrows
-keymap("n", "<C-Up>", ":resize -2<CR>", opts)
-keymap("n", "<C-Down>", ":resize +2<CR>", opts)
-keymap("n", "<C-Left>", ":vertical resize -2<CR>", opts)
-keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)
+keymap("n", "<C-M-j>", ":resize -2<CR>", opts)
+keymap("n", "<C-M-k>", ":resize +2<CR>", opts)
+keymap("n", "<C-M-l>", ":vertical resize -2<CR>", opts)
+keymap("n", "<C-M-h>", ":vertical resize +2<CR>", opts)
 
 -- up/down line
 keymap("n", "<M-j>", "ddp", opts) -- move line below
@@ -103,6 +103,31 @@ keymap("i", "<M-l>", "<Right>", opts)
 
 
 ---------------------- Plugins ----------------------
+
+-- bufferline
+keymap('n', '<leader>1', '<Cmd>BufferLineGoToBuffer 1<CR>', opts)
+keymap('n', '<leader>2', '<Cmd>BufferLineGoToBuffer 2<CR>', opts)
+keymap('n', '<leader>3', '<Cmd>BufferLineGoToBuffer 3<CR>', opts)
+keymap('n', '<leader>4', '<Cmd>BufferLineGoToBuffer 4<CR>', opts)
+keymap('n', '<leader>5', '<Cmd>BufferLineGoToBuffer 5<CR>', opts)
+keymap('n', '<leader>6', '<Cmd>BufferLineGoToBuffer 6<CR>', opts)
+keymap('n', '<leader>7', '<Cmd>BufferLineGoToBuffer 7<CR>', opts)
+keymap('n', '<leader>8', '<Cmd>BufferLineGoToBuffer 8<CR>', opts)
+keymap('n', '<leader>9', '<Cmd>BufferLineGoToBuffer 9<CR>', opts)
+keymap('n', '<leader>$', '<Cmd>BufferLineGoToBuffer -1<CR>', opts)
+
+-- code-runner
+keymap('n', '<leader>r', ':RunCode<CR>', opts)
+keymap('n', '<leader>rf', ':RunFile<CR>',opts)
+keymap('n', '<leader>rft', ':RunFile tab<cr>', opts)
+keymap('n', '<leader>rp', ':RunProject<Cr>', opts)
+keymap('n', '<leader>rc', ':runclose<cr>', opts)
+keymap('n', '<leader>crf', ':CRFiletype<cr>', opts)
+keymap('n', '<leader>crp', ':CRProjects<cr>', opts)
+
+-- null-ls
+keymap("n", "gl", "<cmd>lua vim.diagnostic.open_float()<CR>", opts)
+keymap("n", "<leader>lf", "<cmd>lua vim.lsp.buf.formatting()<cr>", opts)
 
 -- NvimTree
 keymap("n", "<leader>e", ":NvimTreeToggle<CR>", opts)
