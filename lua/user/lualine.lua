3063c5a47ff28fab13093a2d2a132c866f9bf119
Jorge Rodriguez <jarf4321@gmail.com>
2022-08-20 18:12:56 -0500

    Change - Delete default icon on command change and current command display

--- lua/user/lualine.lua
+++ lua/user/lualine.lua
@@ -86,15 +86,46 @@ ins_left {
   padding = { left = 0, right = 1 }, -- We don't need space before this
 }
 
+-- ins_left {
+--   -- mode component
+--   function()
+--     return ''
+--   end,
+--   color = function()
+--     -- auto change color according to neovims mode
+--     local mode_color = {
+--       n = colors.red,
+--       i = colors.green,
+--       v = colors.blue,
+--       [''] = colors.blue,
+--       V = colors.blue,
+--       c = colors.magenta,
+--       no = colors.red,
+--       s = colors.orange,
+--       S = colors.orange,
+--       [''] = colors.orange,
+--       ic = colors.yellow,
+--       R = colors.violet,
+--       Rv = colors.violet,
+--       cv = colors.red,
+--       ce = colors.red,
+--       r = colors.cyan,
+--       rm = colors.cyan,
+--       ['r?'] = colors.cyan,
+--       ['!'] = colors.red,
+--       t = colors.red,
+--     }
+--     return { fg = mode_color[vim.fn.mode()] }
+--   end,
+--   padding = { right = 1 },
+-- }
+
 ins_left {
-  -- mode component
-  function()
-    return ''
-  end,
+  'mode',
   color = function()
     -- auto change color according to neovims mode
     local mode_color = {
-      n = colors.red,
+      n = colors.white,
       i = colors.green,
       v = colors.blue,
       [''] = colors.blue,
@@ -117,7 +148,6 @@ ins_left {
     }
     return { fg = mode_color[vim.fn.mode()] }
   end,
-  padding = { right = 1 },
 }
 
 ins_left {
@@ -146,6 +176,7 @@ ins_left {
     color_info = { fg = colors.cyan },
   },
 }
+
 
 -- Insert mid section. You can make any number of sections in neovim :)
 -- for lualine it's any number greater then 2
